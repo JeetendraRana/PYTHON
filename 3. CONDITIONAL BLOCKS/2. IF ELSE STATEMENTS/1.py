87c123540763bfd9212ba0b574df3f206b070271
Jeetendra Kumar Rana <125862464+JeetendraRana@users.noreply.github.com>
2025-01-15 11:15:28 +0000

    Program based on if else

---
+++ 3. CONDITIONAL BLOCKS/2. IF ELSE STATEMENTS/1.py
@@ -0,0 +1,8 @@
+# program to check a person is voter or non voter
+age=int(input("Enter your current age: "))
+print("Your current age is: ",age)
+if age>=18:
+    print("Congratulatins! You are a voter now.")
+else:
+    print("Sorry! You are not eligible for a vote")
+    
