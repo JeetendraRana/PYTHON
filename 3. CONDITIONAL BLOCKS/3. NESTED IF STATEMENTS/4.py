87dcf4e93f368490474cb576e11b970ae5f0581b
Jeetendra Kumar Rana <125862464+JeetendraRana@users.noreply.github.com>
2025-01-17 08:31:09 +0000

    Add a program based on nested if statement

---
+++ 3. CONDITIONAL BLOCKS/3. NESTED IF STATEMENTS/4.py
@@ -0,0 +1,16 @@
+#  PROGRAM TO PRINT THE LARGEST OF THE THREE NUMBERS USING NESTED IF STATEMENT.
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number: "))
+print("Number1: ",num1)
+print("Number2: ",num2)
+print("Number3: ",num3)
+if num1>num2:
+    if num1>num3:
+        print("Number1 is greater")
+if num2>num1:
+    if num1>num2:
+        print("Number2 is greater")
+if num3>num1:
+    if num3>num2:
+        print("Number 3 is greater")
