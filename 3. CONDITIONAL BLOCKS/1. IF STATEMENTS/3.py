343ca2f66a57ffc508838292b1def78886b5ccdb
Jeetendra Kumar Rana <125862464+JeetendraRana@users.noreply.github.com>
2025-02-14 09:44:25 +0000

    two programs are added

---
+++ 3. CONDITIONAL BLOCKS/1. IF STATEMENTS/3.py
@@ -0,0 +1,14 @@
+# 1. PROGRAM TO CHECK POSITIVE NUMBER OR NOT
+# n=int(input("Enter a number: "))
+# if n>0:
+#     print("Entered Number is positive: ",n)
+
+# 2. PROGRAM TO CHECK A NUMBER IS DIVISIBLE BY 5
+# n=int(input("Enter a number: "))
+# if n%5==0:
+#     print("Entered number",n, "is divisible by 5")
+
+# 3. CHECK IF A STRING CONTAINS "PYTHON"
+text=input("Enter a text: ")
+if "Python" in text:
+    print("The sentence contains 'Python'")
